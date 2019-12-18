0f9ec38cb31117e901f32f8aa9b4c9e525d11dd9
Rodion Pedyuk <rodionpedyuk@gmail.com>
2019-12-18 12:06:53 +0200

    Updates

---
+++ precache-manifest.df358698dc8a21d643742dc3e21dcbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7244b38351c5109a7208849341a817f3",
+    "url": "/RodionPedyuk/goit-react/index.html"
+  },
+  {
+    "revision": "923142896b2f049178e0",
+    "url": "/RodionPedyuk/goit-react/static/css/main.ef153489.chunk.css"
+  },
+  {
+    "revision": "5b2d6e0536a482f041b8",
+    "url": "/RodionPedyuk/goit-react/static/js/2.11e758ed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RodionPedyuk/goit-react/static/js/2.11e758ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "923142896b2f049178e0",
+    "url": "/RodionPedyuk/goit-react/static/js/main.a523d8ca.chunk.js"
+  },
+  {
+    "revision": "f09efd1763b48d244a58",
+    "url": "/RodionPedyuk/goit-react/static/js/runtime-main.a156b9f0.js"
+  }
+]);
