8c29ac45539ad2efcdc2c4122b935b0ac982d349
Rodion Pedyuk <rodionpedyuk@gmail.com>
2020-01-17 10:23:33 +0200

    Update phonebook.js

--- src/components/phonebook/phonebook.js
+++ src/components/phonebook/phonebook.js
@@ -7,16 +7,28 @@ const itemId = require('uuid/v4');
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
   };
+
+  componentDidMount() {
+    // localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+
+    this.setState(prevState => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          ...JSON.parse(localStorage.getItem('contacts')),
+        ],
+      };
+    });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   getValue = e => {
     const { name, value } = e.target;
